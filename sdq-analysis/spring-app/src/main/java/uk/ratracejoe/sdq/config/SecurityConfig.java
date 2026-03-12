f3d2f347eda50dac6a38cb85bfabda592d1d8f8a
Joe Sharp <ratracejoe@hotmail.com>
2026-03-12 20:42:13 +0000

    auth working a bit better, switched to just over make

--- sdq-analysis/spring-app/src/main/java/uk/ratracejoe/sdq/config/SecurityConfig.java
+++ sdq-analysis/spring-app/src/main/java/uk/ratracejoe/sdq/config/SecurityConfig.java
@@ -1,20 +1,26 @@
 package uk.ratracejoe.sdq.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(
-      HttpSecurity http, ClientRegistrationRepository clientRegRepo) throws Exception {
+      HttpSecurity http, OAuth2AuthorizedClientService clientService) throws Exception {
     http.csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
         .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("/", true)) // enables Cognito login
@@ -22,10 +28,35 @@ public class SecurityConfig {
             logout ->
                 logout.logoutSuccessHandler(
                     (req, res, auth) -> {
+                      OAuth2AuthenticationToken oauth = (OAuth2AuthenticationToken) auth;
+
+                      OAuth2AuthorizedClient client =
+                          clientService.loadAuthorizedClient(
+                              oauth.getAuthorizedClientRegistrationId(), oauth.getName());
+
+                      ClientRegistration reg = client.getClientRegistration();
+
+                      String logoutUri =
+                          (String)
+                              reg.getProviderDetails()
+                                  .getConfigurationMetadata()
+                                  .get("end_session_endpoint");
+
+                      String baseUrl =
+                          UriComponentsBuilder.fromUriString(req.getRequestURL().toString())
+                              .replacePath(req.getContextPath())
+                              .replaceQuery(null)
+                              .build()
+                              .toUriString();
+
                       String redirect =
-                          "http://localhost:8085/realms/ratracejoe/protocol/openid-connect/logout"
-                              + "?post_logout_redirect_uri=http://localhost:8080/"
-                              + "&client_id=timesheets-service";
+                          logoutUri
+                              + "?post_logout_redirect_uri="
+                              + baseUrl
+                              + "&client_id="
+                              + reg.getClientId();
+
+                      log.info("Logout Redirect " + redirect);
                       res.sendRedirect(redirect);
                     }));
 
